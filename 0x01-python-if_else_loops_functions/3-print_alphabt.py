8b2f0666bbf5eb3ec4a6050d5ef10f91f4ccf6ec
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-05-31 22:14:05 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if c != ord('e') and c != ord('q'):
-        print("{:c}".format(c), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
