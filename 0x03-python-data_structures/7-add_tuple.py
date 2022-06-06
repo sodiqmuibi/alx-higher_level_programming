aaf3bd9d67e5b72478de284a9c7786473fdc1f7d
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-06 13:28:59 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return 
