8bd853bb09002e02282b38f977d4cafdfa206c0e
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-06 13:18:55 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return 'None'
+    else:
+        return my_list[idx]
