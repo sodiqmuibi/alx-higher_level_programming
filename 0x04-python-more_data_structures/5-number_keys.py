3a501bcabe0ebc9130d2ff0af5ce8a6757ee899d
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-09 10:27:45 +0100

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+def number_keys(a_dictionary):
+    return len(a_dictionary.keys())
