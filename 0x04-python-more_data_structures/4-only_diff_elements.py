00d5bfd381be8569aebd1f1cdb05472b7577ead6
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-09 10:26:22 +0100

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
