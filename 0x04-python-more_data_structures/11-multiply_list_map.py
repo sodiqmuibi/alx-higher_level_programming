ceeb6f0133b747ccc933db7c25c14d6ebb92ff85
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-09 10:51:43 +0100

    Update 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+def mutiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
