8e9d3289709507c48ec342e1fb7d093af56410bb
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-09 11:20:45 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary)
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
