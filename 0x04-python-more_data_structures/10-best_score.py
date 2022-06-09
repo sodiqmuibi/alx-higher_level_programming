22a8cdcf0e8b0b71c0e4b4d7b8506055bca7f04d
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-09 10:50:46 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    return max(a_dictionary)
