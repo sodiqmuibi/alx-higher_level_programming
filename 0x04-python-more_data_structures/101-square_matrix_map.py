3f19cf00b45804c9a22453ed13d45a6cdbf4fedc
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-09 11:18:11 +0100

    Update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary)
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
