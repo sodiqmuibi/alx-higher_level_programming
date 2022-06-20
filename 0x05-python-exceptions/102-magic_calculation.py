7fcd7701648b4a25e3959ff061f972ecd9fc2191
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-20 17:25:59 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(0, x):
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
         except:
-            pass
-    print()
-    return count
+            result = b + a
+            break
+    return result
