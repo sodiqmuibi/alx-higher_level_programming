b5c1d18dc38777b10249f87d627fb415b85de217
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-20 17:19:07 +0100

    Update 5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except:
-            pass
-    print()
-    return count
+def raise_exception():
+    raise TypeError
