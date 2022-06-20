f7e1167f485c9014e541a4bce1679d3567a005c3
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-20 17:15:39 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except:
-            pass
-    print()
-    return count
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
