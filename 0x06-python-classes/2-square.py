19c4d87eb18b24b910b472e399dfffce2cbd0346
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-06-22 19:45:34 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+'''Module doc'''
+
+
 class Square:
     """Represents a square.
     Private instance attribute: size.
-    Instantiation with size (no type/value verification).
+    Instantiation with optional size.
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
